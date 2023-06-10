aecea43059f413e68c9499dcdbf049d7583e24f2
kei-1313 <candydog666@gmail.com>
2023-06-10 11:00:01 +0900

    スタートでのarryの使い方

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Game from './component/Game';
 import Obj from './component/Obj';
+import Arry from './component/Arry';
 function App() {
   return (
     <div className="App">
       <Game />
       <Obj/>
+      <Arry/>
     </div>
   );
 }
--- src/component/Arry.jsx
+++ src/component/Arry.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import {useState} from 'react'
 
 const Arry = () => {
+  const numArray = [1, 2, 3, 4, 5];
+  const [nums, setNums] = useState(numArray);
+
+  const changeNum = () => { 
+    const newNums = [...nums]
+    const value = newNums.pop();
+    newNums.unshift(value);
+    setNums(newNums);
+  }
+
   return (
     <div className='tutorial'>
-        
+      <div>{nums}</div>
+      <button onClick={changeNum}>クリック</button>
     </div>
   )
 }
