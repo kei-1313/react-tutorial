9aaada6896ceb20a3800a226acbaf2726543450e
kei-1313 <candydog666@gmail.com>
2023-06-10 19:14:03 +0900

    リファクタリング

--- src/component/FormTodo/FormTodo.jsx
+++ src/component/FormTodo/FormTodo.jsx
@@ -4,10 +4,16 @@ import List from './List'
 const FormTodo = () => {
   const tasksData = [{id: 1, name: "宿題をする"},{id: 2, name: "ジムに行く"},{id: 3, name: "買い物に行く"}]
   const [tasks, setTasks] = useState(tasksData);
+
   const [val, setVal] = useState("");
+  
   const addTask = () => {
     const newTasks = [...tasks];
-    setTasks([...newTasks, {id: Math.floor(Math.random() * 10000),name: val}]);
+    const newTask = {
+      id: Math.floor(Math.random() * 10000),
+      name: val
+    }
+    setTasks([...newTasks, newTask]);
     setVal('')
   }
   const completeTask = (id) => {
