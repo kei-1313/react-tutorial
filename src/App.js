2e48cee63aad51d2f10eaa5b1eca24f7f01796c4
kei-1313 <candydog666@gmail.com>
2023-06-10 10:25:07 +0900

    tutorialコンポーネント追加

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Game from './component/Game';
+import Tutorial from './component/Tutorial';
 function App() {
   return (
     <div className="App">
       <Game />
+      <Tutorial/>
     </div>
   );
 }
