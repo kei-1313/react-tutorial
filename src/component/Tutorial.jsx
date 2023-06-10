7e0c33ceccd6438ad74cb65d4063e4e8ab77afb2
kei-1313 <candydog666@gmail.com>
2023-06-10 10:50:05 +0900

    ステートでオブジェクトを使う

--- src/component/Tutorial.jsx
+++ src/component/Tutorial.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Tutorial = () => {
+
+  const [person, setPerson] = useState({name: 'Kei', age: 24});
+
+  const handlePerson = (e) => {
+    setPerson(() => ({...person, name: e.target.value}));
+  }
+
+  const countUp = () => { 
+    setPerson(() => ({...person, age: person.age + 1}));
+  }
+
+  const countDown = () => { 
+    setPerson(() => ({...person, age: person.age - 1}));
+  }
+
+
   return (
-    <div className='tutorial'>Tutorial</div>
+    <div className='tutorial'>
+      <div>
+        <div>Name: {person.name}</div>
+        <div>age: {person.age}</div>
+        <input type="text" onChange={handlePerson}/>
+        <button onClick={countUp}>+</button>
+        <button onClick={countDown}>-</button>
+      </div>
+    </div>
   )
 }
 
