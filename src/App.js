ef3377e37bfdec094dcdae55707444fa635a7b8d
kei-1313 <candydog666@gmail.com>
2023-06-10 14:54:28 +0900

    countコンポーネントの作成

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Game from './component/Game';
 import Obj from './component/Obj';
 import Arry from './component/Arry';
+import Count from './component/Count';
 function App() {
   return (
     <div className="App">
       <Game />
       <Obj/>
       <Arry/>
+      <Count/>
     </div>
   );
 }
---
+++ src/component/Count.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Count = () => {
+  return (
+    <div className='tutorial'>
+      <div>count :</div>
+      <button ></button>
+    </div>
+  )
+}
+
+export default Count
