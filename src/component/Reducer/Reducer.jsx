b304f242b86ea45a07359cffb7e7220c25289f69
kei-1313 <candydog666@gmail.com>
2023-06-11 17:25:22 +0900

    useReducerの使い方

--- src/component/Reducer/Reducer.jsx
+++ src/component/Reducer/Reducer.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
+import {useReducer, useState} from 'react'
 
 const Reducer = () => {
+  const [val, setVal] = useState(0);
+  const [rval, dispatch] = useReducer((prev, {type, step}) => {
+    switch (type) {
+      case "+":
+        return prev + step;
+      case "-":
+        return prev - step;
+      default:
+        throw new Error('不明なactionです')
+    }
+    // if(action === "+") {
+    //   return ++ prev;
+    // } else if (action === "-") {
+    //   return --prev;
+    // }
+  }, 0);
+
+  const countUp = () => { 
+    setVal(prev => ++prev)
+  }
+
+  const rcountUp = () => { 
+    dispatch({type: "+", step: 10});
+  }
+
+  const rcountDown= () => { 
+    dispatch({type: "-", step: 5});
+  }
   return (
-    <div className='tutorial'>
-      <div>Reducer</div>
-    </div>
+    <>
+      <div className='tutorial'>
+        <h3>{val}</h3>
+        <button onClick={countUp}>増やす</button>
+      </div>
+      <div className='tutorial'>
+        <h3>{rval}</h3>
+        <div>
+          <button onClick={rcountUp}>UP</button>
+        </div>
+        <div>
+          <button onClick={rcountDown}>Down</button>
+        </div>
+      </div>
+    </>
   )
 }
 
