9e0f8f0aaaccc38c2841197ff6b508785bef9b50
kei-1313 <candydog666@gmail.com>
2023-06-15 01:01:21 +0900

    useEffectを使ってみる02

--- src/component/effect/Effect.jsx
+++ src/component/effect/Effect.jsx
@@ -2,17 +2,32 @@ import React, { useEffect, useState } from 'react'
 
 const Effect = () => {
   const [time, setTime] = useState(0);
+  const [status, setStatus] = useState(false)
 
   useEffect(() => {
-    window.setInterval(() => {
-      setTime(prev => prev + 1);
-    }, 1000)
+    console.log("call back");
+    // window.setInterval(() => {
+    //   setTime(prev => prev + 1);
+    // }, 1000)
   }, [])
+
+  useEffect(() => {
+    if(status) {
+      window.alert("アラートです");
+    }
+  }, [status])
 
   return (
     <div className='tutorial'>
       <h3>{time}</h3>
       <span>秒経過</span>
+
+      <div className="check">
+        <label htmlFor="check">
+          <input id='check' type={"checkbox"} checked={status} onClick={() => setStatus((status) => !status)}/>
+          click this
+        </label>
+      </div>
     </div>
   )
 }
