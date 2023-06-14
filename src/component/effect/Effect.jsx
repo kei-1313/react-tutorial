c65ee811a222cca24ad449ac7fe5c8271d52b5bd
kei-1313 <candydog666@gmail.com>
2023-06-15 01:15:41 +0900

    クリーンアップ関数を使ってみる

--- src/component/effect/Effect.jsx
+++ src/component/effect/Effect.jsx
@@ -1,8 +1,29 @@
 import React, { useEffect, useState } from 'react'
 
+const End = () => {
+  useEffect(() => {
+    console.log("start");
+    // 依存配列を設定してなければ、コンポーネントが削除された段階で発火
+    return() => {
+      console.log("end");
+    }
+  })
+  return (
+    <>
+      <div>
+        エンド
+      </div>
+      
+    </>
+  )
+}
+
+
+
 const Effect = () => {
   const [time, setTime] = useState(0);
   const [status, setStatus] = useState(false)
+  const [isDisplay, setIsDisplay] = useState(true);
 
   useEffect(() => {
     console.log("call back");
@@ -15,6 +36,8 @@ const Effect = () => {
     if(status) {
       window.alert("アラートです");
     }
+
+    
   }, [status])
 
   return (
@@ -28,6 +51,9 @@ const Effect = () => {
           click this
         </label>
       </div>
+
+      {isDisplay && <End/>}
+      <button onClick={() => setIsDisplay(prev => !prev)}>変更</button>
     </div>
   )
 }
