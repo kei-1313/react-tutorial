d9c5c9f579aa127d73bdf8383c38dff678e4bc0a
kei-1313 <candydog666@gmail.com>
2023-06-16 01:09:04 +0900

    timer完成

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
 
       <ParentContext />
 
-      <Effect/>
+      {/* <Effect/> */}
 
       <Timer/>
     </div>
--- src/component/effect/Timer.jsx
+++ src/component/effect/Timer.jsx
@@ -1,12 +1,31 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Timer = () => {
   const [time, setTime] = useState(0);
+  const [start, setStart] = useState(null);
+
+  useEffect(() => {
+    if(start) {
+      let intervalId = null;
+      intervalId = window.setInterval(() => {
+        setTime(prev => prev + 1);
+      }, 1000)
+      return() => {
+        clearInterval(intervalId);
+        setTime(0)
+      } 
+    } 
+      
+    
+  }, [start])
+
   return (
     <>
       <div className="tutorial">
         <h2>Timer</h2>
         <h3>{time}秒経過</h3>
+        <button onClick={() => setStart(true)}>スタート</button>
+        <button onClick={() => setStart(false)}>リセット</button>
       </div>
     </>
   )
