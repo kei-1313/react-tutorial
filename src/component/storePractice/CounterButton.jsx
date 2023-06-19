06e400e3a8dcc3fa5f9b44e9be4b4f878c273163
kei-1313 <candydog666@gmail.com>
2023-06-19 21:26:55 +0900

    redux復習

--- src/component/storePractice/CounterButton.jsx
+++ src/component/storePractice/CounterButton.jsx
@@ -1,5 +1,7 @@
 import { useDispatch } from "react-redux";
-import { useCounterDispatch } from "./CounterContext";
+// import { useCounterDispatch } from "./CounterContext";
+
+import { add, minus } from "../../store/modules/counter"
 
 const CounterButton = ({ calcType, step }) => {
 
@@ -7,7 +9,10 @@ const CounterButton = ({ calcType, step }) => {
   // const dispatch = useCounterDispatch();
 
   const clickHandler = () => {
-    dispatch({ type: calcType, step });
+    // dispatch({ type: 'counter' / + calcType, payload: step });
+    const action = calcType === '+' ? add(step) : minus(step)
+    console.log(action);
+    dispatch(action)
   }
 
   return <button onClick={clickHandler}>{calcType}{step}</button>
--- src/store/index.js
+++ src/store/index.js
@@ -1,33 +1,7 @@
 import { createStore, combineReducers } from "redux";
-
-const initialState = 0;
-const reducer = (state = initialState, { type, step }) => {
-  console.log(type);
-  switch (type) {
-    case "+":
-      return state + step;
-    case "-":
-      return state - step;
-    default:
-      return state
-  }
-};
-
-const reducer2 = (state = initialState, { type, step }) => {
-  console.log(type);
-  switch (type) {
-    case "+":
-      return state + step;
-    case "-":
-      return state - step;
-    default:
-      return state
-  }
-};
-
+import { reducer } from "./modules/counter";
 const reducers = combineReducers({
   counter: reducer,
-  counter2: reducer2
 })
 
 export default createStore(
