98f2bf933adeab1203607683f6abe06d0ccd435a
kei-1313 <candydog666@gmail.com>
2023-07-06 08:57:38 +0900

    無限ループ解消

--- src/component/user/User.jsx
+++ src/component/user/User.jsx
@@ -1,15 +1,19 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 function User() {
+  const [users, setUsers] = useState([])
+
   useEffect(() => {
     const getUser = async () => {
       //axios内でparseなどを行っている
       const res = await axios.get('http://localhost:3003/user')
+
+      setUsers(res.data);
       console.log(res.data);
     }
     getUser();
-  })
+  }, [])
   return (
     <>
       <div className='tutorial'>
