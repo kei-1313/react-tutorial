f46d482afb2022bb87b1de17fd128031b4e7ffe3
kei-1313 <candydog666@gmail.com>
2023-07-06 08:51:15 +0900

    axiosでリクエストを投げ、jsonを取得

--- src/component/user/User.jsx
+++ src/component/user/User.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
 
 function User() {
+  useEffect(() => {
+    const getUser = async () => {
+      //axios内でparseなどを行っている
+      const res = await axios.get('http://localhost:3003/user')
+      console.log(res.data);
+    }
+    getUser();
+  })
   return (
     <>
       <div className='tutorial'>
